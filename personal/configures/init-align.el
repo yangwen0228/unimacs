4b2ceeedee053dfcc3b61e841c744ca833db5074
yangwen0228 <yangwen0228@foxmail.com>
2016-05-17 15:19:25 +0800

    Bugfix: align-regexp-repeated: indent use the new stop position.

--- personal/configures/init-align.el
+++ personal/configures/init-align.el
@@ -5,9 +5,31 @@
 
 ;;; Code:
 (use-package align
-  :bind ("C-x \\" . align-regexp)
+  :bind (("C-x \\" . align-regexp)
+         ("C-x |" . align-regexp-repeated))
   :preface
-    ;; <item name="test" age ="20"/>
+  (defun align-regexp-repeated (start stop regexp)
+    "Like align-regexp, but repeated for multiple columns. See
+http://www.emacswiki.org/emacs/AlignCommands"
+    (interactive "r\nsAlign regexp: ")
+    (let ((regexp (if (string= regexp "") " " regexp))
+          (spacing 1)
+          (old-buffer-size (buffer-size)))
+      ;; If our align regexp is just spaces, then we don't need any
+      ;; extra spacing.
+      (when (string-match regexp " ")
+        (setq spacing 0))
+      (align-regexp start stop
+                    ;; add space at beginning of regexp
+                    (concat "\\([[:space:]]*\\)" regexp)
+                    1 spacing t)
+      ;; modify stop because align-regexp will add/remove characters
+      (align-regexp start (+ stop (- (buffer-size) old-buffer-size))
+                    ;; add space at end of regexp
+                    (concat regexp "\\([[:space:]]*\\)")
+                    1 0 t)
+      (indent-region start (+ stop (- (buffer-size) old-buffer-size)))))
+  ;; <item name="test" age ="20"/>
   ;; <item name="test2" age ="20"/>
   ;; =>
   ;; <item name="test"  age ="20"/>
@@ -36,7 +58,7 @@
   ;; set a   $b
   ;; set lst $c
   (defun my-align-by-dollar (start end)
-    "Align by space repeat."
+    "Align by $ sign repeat."
     (interactive "r")
     (align-regexp start end "\\(\\s-*\\)\\$"
                   -1 1 t)
