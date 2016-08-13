d9a0a379030aab2fc5f8c61f05fb91f17c3347fe
yangwen0228 <yangwen0228@foxmail.com>
2016-08-13 13:48:40 +0800

    鏂板畫浣?

--- core/unimacs-ui.el
+++ core/unimacs-ui.el
@@ -82,7 +82,7 @@ If set/leave chinese-font-size to nil, it will follow english-font-size"
 
   (require 'cl) ; for find if
   (let* ((english-fonts '("Consolas" "Monaco" "DejaVu Sans Mono" "Monospace" "Courier New"))
-         (chinese-fonts '("Microsoft Yahei" "文泉驿等宽微米黑" "黑体" "新宋体" "宋体"))
+         (chinese-fonts '("新宋体" "黑体" "Microsoft Yahei" "文泉驿等宽微米黑" "宋体"))
          (en-font (unimacs-make-font-string (find-if #'x-list-fonts english-fonts)
                                           unimacs-font-size))
          (zh-font (font-spec :family (find-if #'x-list-fonts chinese-fonts)
