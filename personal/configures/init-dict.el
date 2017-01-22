fb25344665f4eff48007dd4850b2d1b4f5ecaf4a
yangwen0228 <yangwen0228@foxmail.com>
2017-01-22 14:41:24 +0800

    Add dict support.

---
+++ personal/configures/init-dict.el
@@ -0,0 +1,10 @@
+;;; init-dict.el --- Summary
+;;; Commentary:
+;; comments
+
+;;; Code:
+(use-package youdao-dictionary
+  :commands (youdao-dictionary-search-at-point youdao-dictionary-search-from-input))
+
+(provide 'init-dict)
+;;; init-dict.el ends here
