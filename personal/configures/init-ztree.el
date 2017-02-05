5583c1425a70e64e4361eab367c5d968f4e3f1fa
yangwen0228 <yangwen0228@foxmail.com>
2017-02-05 21:12:28 +0800

    ztree diff and dir.

---
+++ personal/configures/init-ztree.el
@@ -0,0 +1,11 @@
+;;; init-ztree.el --- Summary
+;;; Commentary:
+;; comments
+
+;;; Code:
+(use-package ztree
+  :commands (ztree-diff ztree-dir)
+  )
+
+(provide 'init-ztree)
+;;; init-ztree.el ends here
