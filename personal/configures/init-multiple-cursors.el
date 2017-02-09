d9c1d77d87dbb444784ba939885e6d96072a43b4
yangwen0228 <yangwen0228@foxmail.com>
2017-02-09 23:34:15 +0800

    Add mouse click to add/delete cursors.

--- personal/configures/init-multiple-cursors.el
+++ personal/configures/init-multiple-cursors.el
@@ -1,16 +1,20 @@
 ;;; init-multiple-cursors.el --- Summary
 ;;; Commentary:
 ;; IMPORTANT: multiple-cursors use ~/.emacs.d/.mc-lists.el to keep choose settings.
+;; https://github.com/magnars/multiple-cursors.el
 
 ;;; Code:
 (use-package multiple-cursors
-  :bind (("C->"     . mc/mark-next-like-this)
-         ("C-<"     . mc/mark-previous-like-this)
-         ("C-M->"   . mc/skip-to-next-like-this)
-         ("C-M-<"   . mc/skip-to-previous-like-this)
-         ("C-c C-<" . mc/mark-all-like-this))
+  :bind (("C->"           . mc/mark-next-like-this)
+         ("C-<"           . mc/mark-previous-like-this)
+         ("C-M->"         . mc/skip-to-next-like-this)
+         ("C-M-<"         . mc/skip-to-previous-like-this)
+         ("C-c C-<"       . mc/mark-all-like-this)
+         ("C-S-<mouse-1>" . mc/add-cursor-on-click)
+         :map mc/keymap
+         ("C-|" . mc/vertical-align-with-space))
   :config
-  )
+  (setq mc/insert-numbers-default 1))
 
 (provide 'init-multiple-cursors)
 ;;; init-multiple-cursors.el ends here
