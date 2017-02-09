8e1510cd9efe88c5f6c4daf31a094c7af8c86ffa
yangwen0228 <yangwen0228@foxmail.com>
2017-02-09 15:48:11 +0800

    Add 2 binds to mark and delete sp-sexp.

--- personal/configures/init-smartparens.el
+++ personal/configures/init-smartparens.el
@@ -8,6 +8,8 @@
          ("C-M-b" . sp-backward-sexp)
          ("C-M-n" . sp-next-sexp)
          ("C-M-p" . sp-previous-sexp)
+         ("C-M-S-SPC" . sp-mark-sexp)
+         ("C-M-S-k"   . sp-kill-sexp)
          ("M-[" .   sp-unwrap-sexp)
          ("M-]" .   sp-backward-unwrap-sexp)
          ("C-M-{" . sp-rewrap-sexp))
@@ -19,6 +21,29 @@
   (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
   (add-hook 'lisp-mode-hook 'turn-on-smartparens-strict-mode)
   (add-hook 'markdown-mode-hook 'turn-on-smartparens-strict-mode)
+
+  ;; @ref https://github.com/Fuco1/.emacs.d/blob/master/files/smartparens.el
+  ;; @doc https://github.com/Fuco1/smartparens/wiki/Pair-management
+  ;;; org-mode
+  (sp-with-modes 'org-mode
+    (sp-local-pair "*" "*" :actions '(insert wrap) :unless '(sp-point-after-word-p sp-point-at-bol-p) :wrap "C-*" :skip-match 'sp--org-skip-asterisk)
+    (sp-local-pair "_" "_" :unless '(sp-point-after-word-p))
+    (sp-local-pair "/" "/" :unless '(sp-point-after-word-p) :post-handlers '(("[d1]" "SPC")))
+    (sp-local-pair "~" "~" :unless '(sp-point-after-word-p) :post-handlers '(("[d1]" "SPC")))
+    (sp-local-pair "=" "=" :unless '(sp-point-after-word-p) :post-handlers '(("[d1]" "SPC")))
+    (sp-local-pair "?" "?"))
+
+  (defun sp--org-skip-asterisk (ms mb me)
+    (or (and (= (line-beginning-position) mb)
+             (eq 32 (char-after (1+ mb))))
+        (and (= (1+ (line-beginning-position)) me)
+             (eq 32 (char-after me)))))
+
+  ;;; tcl-mode
+  (sp-with-modes 'tcl-mode
+    (sp-local-pair "hwi OpenStack" "hwi CloseStack")
+    (sp-local-pair "OpenStack" "CloseStack"))
+
   :diminish (smartparens-mode smartparens-global-mode))
 
 (provide 'init-smartparens)
