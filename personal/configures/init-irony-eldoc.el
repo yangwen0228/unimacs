4ec9ddddcf1b6c251448e04df4e68a24d73984fb
yangwen0228 <yangwen0228@foxmail.com>
2017-02-13 21:29:00 +0800

    remove irony-eldoc.

--- personal/configures/init-irony-eldoc.el
+++
@@ -1,9 +0,0 @@
-;;; init-irony-eldoc.el --- configure for irony eldoc
-;;; Commentary:
-;; use irony and eldoc to show the function definitions.
-
-;;; Code:
-(add-hook 'irony-mode-hook 'irony-eldoc)
-
-(provide 'init-irony-eldoc)
-;;; init-irony-eldoc.el ends here
