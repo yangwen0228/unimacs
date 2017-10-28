8acd1399959c1cf99c0f2ef4d5137702f0623223
yangwen0228 <yangwen0228@foxmail.com>
2017-10-28 14:42:07 +0800

    Don't use neotree, use speedbar instead.

---
+++ personal/configures/init-sr-speedbar.el
@@ -0,0 +1,14 @@
+;;; init-sr-speedbar.el --- Summary
+;;; Commentary:
+;; comments
+
+;;; Code:
+(use-package sr-speedbar
+  :commands sr-speedbar-open
+  :init
+  (global-set-key [f8] 'sr-speedbar-toggle)
+  :config
+  )
+
+(provide 'init-sr-speedbar)
+;;; init-sr-speedbar.el ends here
